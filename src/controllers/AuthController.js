6c7b909056095b05488c89a89f5b35169e36444e
Hilja P <143746544+h-mp@users.noreply.github.com>
2026-01-09 23:14:47 +0200

    Add login and logout functionality, tweak registration

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -7,11 +7,97 @@
 
 import bcrypt from 'bcryptjs'
 import { db } from '../config/mysql_config.js' 
+import validator from 'validator'
 
 /**
  * Encapsulates a controller.
  */
 export class AuthController {
+  /**
+   * Displays a login form.
+   *
+   * @param {object} req  - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  login (req, res, next) {
+    if (req.session.userId) {
+      // If the user is logged in, redirect them to the home page.
+      res.redirect(`/`)
+    } else {
+      res.render('./auth/login')
+    }
+  }
+
+  /**
+   * Handles a login form submission.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {object} next - Express next middleware function.
+   */
+  async loginPost (req, res, next) {
+    try {
+      const { email, password } = req.body
+
+      // If the username or password is not provided, throw an error.
+      if (!email || !password) {
+        throw new Error('Email and password are required.')
+      }
+
+      // Validate email format
+      if (!validator.isEmail(email)) {
+        throw new Error('Invalid email format.')
+      }
+
+      // Retrieve the user from the database.
+      const query = 'SELECT userid, password FROM Members WHERE email = ?'
+      const [results] = await db.query(query, [email])
+      const user = results[0]
+
+      // Compare passwords.
+      const isPasswordValid = await bcrypt.compare(password, user.password)
+
+      if (!isPasswordValid) {
+        throw new Error('Invalid email or password.')
+      }
+
+      // Regenerate the session and redirect the user to the home page.
+      req.session.regenerate(async () => {
+        req.session.userId = user.userid
+        res.redirect(`/`)
+      })
+    } catch (error) {
+      res.locals.errorMessage = 'Invalid email or password.'
+      res.render('./auth/login')
+    }
+  }
+
+  /**
+   * Logs out the user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  logout (req, res, next) {
+    if (req.session.userId) {
+      // If the user is logged in, destroy the session.
+      req.session.destroy(err => {
+        if (err) {
+          return next(err)
+        }
+
+        // Redirect the user to the home page.
+        res.redirect(`/`)
+      })
+    } else {
+      const error = new Error('Not Found')
+      error.status = 404
+      next(error)
+    }
+  }
+
   /**
    * Displays a registration form.
    *
@@ -22,7 +108,7 @@ export class AuthController {
   register (req, res, next) {
     if (req.session.userId) {
       // If the user is logged in, redirect them to the home page.
-      res.redirect(`${app.locals.baseURL}`)
+      res.redirect(`/`)
     } else {
       res.render('./auth/register')
     }
@@ -41,12 +127,17 @@ export class AuthController {
 
       // If the username or password is not provided, throw an error.
       if (!fname || !lname || !address || !city || !zip || !email || !password) {
-      res.locals.errorMessage = 'All required fields must be filled.'
-      return res.render('./auth/register')
+        res.locals.errorMessage = 'All required fields must be filled.'
+        return res.render('./auth/register')
       }
-    
+      // Validate email format
+      if (!validator.isEmail(email)) {
+        res.locals.errorMessage = 'Invalid email format.'
+        return res.render('./auth/register')
+      }
+
       // Validate password length
-      if (password.length < 6) {
+      if (validator.isLength(password, { min: 6, max: 150 })) {
         res.locals.errorMessage = 'Password must be at least 6 characters long.'
         return  res.render('./auth/register')
       }
@@ -62,7 +153,7 @@ export class AuthController {
       // Insert the new member into the database.
       const query = `INSERT INTO Members (fname, lname, address, city, zip, phone, email, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
 
-      const [results] = await db.query(query, [fname, lname, address, city, zip, phone, email, hashedPassword])
+      await db.query(query, [fname, lname, address, city, zip, phone, email, hashedPassword])
 
       res.locals.successMessage = 'Account created successfully. You can now log in.'
       res.render('./auth/register')
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -12,6 +12,13 @@ export const router = express.Router()
 
 const controller = new AuthController()
 
+// Routes for logging in.
+router.get('/login', (req, res, next) => controller.login(req, res, next))
+router.post('/login', (req, res, next) => controller.loginPost(req, res, next))
+
+// Routes for logging out.
+router.get('/logout', (req, res, next) => controller.logout(req, res, next))
+
 // Routes for registering.
 router.get('/register', (req, res, next) => controller.register(req, res, next))
 router.post('/register', (req, res, next) => controller.registerPost(req, res, next))
